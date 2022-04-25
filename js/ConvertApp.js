b6cce828c2d393a7b32fd5ce40696c6933aa88a4
Luke Vo <contact@lukevo.com>
2022-04-25 18:28:19 -0500

    Added saving feature.

--- js/ConvertApp.js
+++ js/ConvertApp.js
@@ -2,44 +2,52 @@
 class ConvertApp extends HTMLElement {
     constructor() {
         super();
-        this.txtChrJson = this.querySelector(".txt-chrome");
-        this.txtKValueJson = this.querySelector(".txt-key-value");
+        this.txtChrJson = this.querySelector("#txt-chrome");
+        this.txtKValueJson = this.querySelector("#txt-key-value");
         this.querySelector(".btn-to-i18n")?.addEventListener("click", () => this.convertToChr());
         this.querySelector(".btn-to-key-value")?.addEventListener("click", () => this.convertToKValue());
+        this.querySelectorAll("textarea[id]").forEach(txt => {
+            const v = localStorage.getItem(txt.id);
+            if (v) {
+                txt.value = v;
+            }
+        });
+    }
+    convert(txtIn, txtOut, transform) {
+        const input = txtIn.value;
+        if (input.length < 10000) {
+            localStorage.setItem(txtIn.id, input);
+        }
+        let json;
+        try {
+            json = JSON.parse(input);
+        }
+        catch (e) {
+            alert("Invalid input JSON: " + e);
+            return;
+        }
+        transform(json);
+        const outJson = JSON.stringify(json, undefined, 4);
+        txtOut.value = outJson;
+        if (outJson.length < 10000) {
+            localStorage.setItem(txtOut.id, outJson);
+        }
     }
     convertToChr() {
-        const input = this.txtKValueJson.value;
-        let json;
-        try {
-            json = JSON.parse(input);
-        }
-        catch (e) {
-            alert("Invalid input JSON: " + e);
-            return;
-        }
-        const out = {};
-        for (const k in json) {
-            out[k] = {
-                message: json[k],
-            };
-        }
-        this.txtChrJson.value = JSON.stringify(out, undefined, 4);
+        this.convert(this.txtKValueJson, this.txtChrJson, json => {
+            for (const k in json) {
+                json[k] = {
+                    message: json[k],
+                };
+            }
+        });
     }
     convertToKValue() {
-        const input = this.txtChrJson.value;
-        let json;
-        try {
-            json = JSON.parse(input);
-        }
-        catch (e) {
-            alert("Invalid input JSON: " + e);
-            return;
-        }
-        const out = {};
-        for (const k in json) {
-            out[k] = json[k].message ?? "";
-        }
-        this.txtKValueJson.value = JSON.stringify(out, undefined, 4);
+        this.convert(this.txtChrJson, this.txtKValueJson, json => {
+            for (const k in json) {
+                json[k] = json[k].message ?? "";
+            }
+        });
     }
     static register() {
         customElements.define("convert-app", this);
--- js/ConvertApp.ts
+++ js/ConvertApp.ts
@@ -1,7 +1,7 @@
 class ConvertApp extends HTMLElement {
 
-    txtChrJson: HTMLInputElement = this.querySelector(".txt-chrome")!;
-    txtKValueJson: HTMLInputElement = this.querySelector(".txt-key-value")!;
+    txtChrJson: HTMLTextAreaElement = this.querySelector("#txt-chrome")!;
+    txtKValueJson: HTMLTextAreaElement = this.querySelector("#txt-key-value")!;
 
     constructor() {
         super();
@@ -10,44 +10,57 @@ class ConvertApp extends HTMLElement {
             () => this.convertToChr());
         this.querySelector(".btn-to-key-value")?.addEventListener("click",
             () => this.convertToKValue());
+
+        this.querySelectorAll<HTMLTextAreaElement>("textarea[id]").forEach(txt => {
+            const v = localStorage.getItem(txt.id);
+            if (v) {
+                txt.value = v;
+            }
+        });
+    }
+
+    private convert(txtIn: HTMLTextAreaElement, txtOut: HTMLTextAreaElement,
+        transform: (json: any) => any) {
+        const input = txtIn.value;
+        if (input.length < 10000) {
+            localStorage.setItem(txtIn.id, input);
+        }
+
+        let json;
+        try {
+            json = JSON.parse(input);
+        } catch (e) {
+            alert("Invalid input JSON: " + e);
+            return;
+        }
+
+        transform(json);
+        
+        const outJson =JSON.stringify(json, undefined, 4) 
+        txtOut.value = outJson;
+        if (outJson.length < 10000) {
+            localStorage.setItem(txtOut.id, outJson);
+        }
     }
 
     convertToChr() {
-        const input = this.txtKValueJson.value;
-        let json;
-        try {
-            json = JSON.parse(input);
-        } catch(e) {
-            alert("Invalid input JSON: " + e);
-            return;
-        }
-
-        const out: any = {};
-        for (const k in json) {
-            out[k] = {
-                message: json[k],
-            }
-        }
-
-        this.txtChrJson.value = JSON.stringify(out, undefined, 4);
+        this.convert(this.txtKValueJson, this.txtChrJson,
+            json => {
+                for (const k in json) {
+                    json[k] = {
+                        message: json[k],
+                    }
+                }
+            });
     }
 
     convertToKValue() {
-        const input = this.txtChrJson.value;
-        let json;
-        try {
-            json = JSON.parse(input);
-        } catch(e) {
-            alert("Invalid input JSON: " + e);
-            return;
-        }
-
-        const out: any = {};
-        for (const k in json) {
-            out[k] = json[k].message ?? "";
-        }
-
-        this.txtKValueJson.value = JSON.stringify(out, undefined, 4);
+        this.convert(this.txtChrJson, this.txtKValueJson,
+            json => {
+                for (const k in json) {
+                    json[k] = json[k].message ?? "";
+                }
+            });
     }
 
     static register() {
